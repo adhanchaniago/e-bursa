fdd724407bb6e13cc1ad80bda52b4bb5d752b6f7
Adi Raka Siwi <adiraka8@gmail.com>
2018-07-16 06:38:48 +0700

    Asdasd

--- database/dbbursa.sql
+++ database/dbbursa.sql
@@ -63,7 +63,7 @@ CREATE TABLE `info_berita` (
 
 /*Data for the table `info_berita` */
 
-insert  into `info_berita`(`id`,`profil_admin_id`,`judul`,`tanggal`,`kategori`,`konten`,`status`) values (1,1,'Lorem Ipsum Dolor Sit Amet','2018-05-28','Berita','<p>Tattatata</p>\r\n<table style=\"border-collapse: collapse; width: 100%;\" border=\"1\">\r\n<tbody>\r\n<tr>\r\n<td style=\"width: 33.3333%;\">asdasd</td>\r\n<td style=\"width: 33.3333%;\">&nbsp;</td>\r\n<td style=\"width: 33.3333%;\">&nbsp;</td>\r\n</tr>\r\n<tr>\r\n<td style=\"width: 33.3333%;\">asd</td>\r\n<td style=\"width: 33.3333%;\">asd</td>\r\n<td style=\"width: 33.3333%;\">&nbsp;</td>\r\n</tr>\r\n<tr>\r\n<td style=\"width: 33.3333%;\">&nbsp;</td>\r\n<td style=\"width: 33.3333%;\">&nbsp;</td>\r\n<td style=\"width: 33.3333%;\">2</td>\r\n</tr>\r\n</tbody>\r\n</table>',1);
+insert  into `info_berita`(`id`,`profil_admin_id`,`judul`,`tanggal`,`kategori`,`konten`,`status`) values (1,1,'Lorem Ipsum Dolor Sit Amet','2018-05-28','Berita','<p>Tattatata</p>\r\n<table style=\"border-collapse: collapse; width: 100%;\" border=\"1\">\r\n<tbody>\r\n<tr>\r\n<td style=\"width: 33.3333%;\">asdasd</td>\r\n<td style=\"width: 33.3333%;\">&nbsp;</td>\r\n<td style=\"width: 33.3333%;\">&nbsp;</td>\r\n</tr>\r\n<tr>\r\n<td style=\"width: 33.3333%;\">asd</td>\r\n<td style=\"width: 33.3333%;\">asd</td>\r\n<td style=\"width: 33.3333%;\">&nbsp;</td>\r\n</tr>\r\n<tr>\r\n<td style=\"width: 33.3333%;\">&nbsp;</td>\r\n<td style=\"width: 33.3333%;\">&nbsp;</td>\r\n<td style=\"width: 33.3333%;\">2</td>\r\n</tr>\r\n</tbody>\r\n</table>',0);
 
 /*Table structure for table `info_komentar` */
 
@@ -91,11 +91,11 @@ CREATE TABLE `lamar` (
   `tanggal_lamar` date NOT NULL,
   `status` tinyint(1) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
 
 /*Data for the table `lamar` */
 
-insert  into `lamar`(`id`,`lowongan_id`,`profil_pencaker_id`,`tanggal_lamar`,`status`) values (2,3,4,'2018-07-14',0);
+insert  into `lamar`(`id`,`lowongan_id`,`profil_pencaker_id`,`tanggal_lamar`,`status`) values (3,3,4,'2018-07-15',0);
 
 /*Table structure for table `lowongan` */
 
@@ -110,6 +110,7 @@ CREATE TABLE `lowongan` (
   `deskripsi_pekerjaan` text NOT NULL,
   `deskripsi_persyaratan` text NOT NULL,
   `gaji` int(11) NOT NULL,
+  `bann` tinyint(1) NOT NULL DEFAULT '0',
   `status` tinyint(1) NOT NULL,
   `dibuat_pada` date NOT NULL,
   PRIMARY KEY (`id`),
@@ -118,7 +119,7 @@ CREATE TABLE `lowongan` (
 
 /*Data for the table `lowongan` */
 
-insert  into `lowongan`(`id`,`profil_perusahaan_id`,`judul`,`tanggal_mulai`,`tanggal_selesai`,`deskripsi_pekerjaan`,`deskripsi_persyaratan`,`gaji`,`status`,`dibuat_pada`) values (3,2,'Web Programmer','2018-07-11','2018-07-14','<p>Saat ini kami sedang membutuhkan staff Web Programmer yang sangat kompeten di bidangnya terutama di bidang API development maupun Website development.</p>\r\n<p><strong>Tanggung Jawab Pekerjaan :</strong></p>\r\n<p>&ndash; Membuat website atau system baru dengan PHP/HTML standard maupun dengan Content Management System dan database MySql.<br />&ndash; Mampu membuat serta mengintegrasikan API (XML/JSON) ke pihak ketiga. (contoh: Hotel Channel Manager dan Hotel Meta Search Engine)<br />&ndash; Maintenance system yang sudah ada (system OTA dan Activities)<br />&ndash; Membuat dan mengembangkan mobile application Android/IOS (Optional)</p>\r\n<p><strong>Tunjangan :</strong></p>\r\n<p>BPJS Kesehatan, BPJS Ketenagakerjaan</p>\r\n<p><strong>Waktu Bekerja :&nbsp;</strong><br />Senin &ndash; Jumat (09:00 &ndash; 17:00), Sabtu (09:00-15:00)</p>\r\n<p>Jenis Pekerjaan: Penuh Waktu</p>','<p><strong>Syarat Pengalaman :&nbsp;</strong><br />Minimal 2 Tahun dibidangnya ataupun Fresh Graduate dengan keahlian khusus.</p>\r\n<p><strong>Keahlian :</strong></p>\r\n<p>Mengusai XML/JSON, HTML, CSS, PHP, MySQL, JavaScript</p>\r\n<p><strong>Kualifikasi :</strong></p>\r\n<p>&ndash; Pria/Wanita usia maksimal 30 tahun<br />&ndash; Mau bekerja keras dan mau berbagi hal-hal baru dibidangnya<br />&ndash; Siap bekerja dalam team maupun sendiri<br />&ndash; Siap ditarget</p>',5000000,1,'2018-07-11');
+insert  into `lowongan`(`id`,`profil_perusahaan_id`,`judul`,`tanggal_mulai`,`tanggal_selesai`,`deskripsi_pekerjaan`,`deskripsi_persyaratan`,`gaji`,`bann`,`status`,`dibuat_pada`) values (3,2,'Web Programmer Junior & Expert','2018-07-11','2018-07-14','<p>Saat ini kami sedang membutuhkan staff Web Programmer yang sangat kompeten di bidangnya terutama di bidang API development maupun Website development.</p>\r\n<p><strong>Tanggung Jawab Pekerjaan :</strong></p>\r\n<p>&ndash; Membuat website atau system baru dengan PHP/HTML standard maupun dengan Content Management System dan database MySql.<br />&ndash; Mampu membuat serta mengintegrasikan API (XML/JSON) ke pihak ketiga. (contoh: Hotel Channel Manager dan Hotel Meta Search Engine)<br />&ndash; Maintenance system yang sudah ada (system OTA dan Activities)<br />&ndash; Membuat dan mengembangkan mobile application Android/IOS (Optional)</p>\r\n<p><strong>Tunjangan :</strong></p>\r\n<p>BPJS Kesehatan, BPJS Ketenagakerjaan</p>\r\n<p><strong>Waktu Bekerja :&nbsp;</strong><br />Senin &ndash; Jumat (09:00 &ndash; 17:00), Sabtu (09:00-15:00)</p>\r\n<p>Jenis Pekerjaan: Penuh Waktu</p>','<p><strong>Syarat Pengalaman :&nbsp;</strong><br />Minimal 2 Tahun dibidangnya ataupun Fresh Graduate dengan keahlian khusus.</p>\r\n<p><strong>Keahlian :</strong></p>\r\n<p>Mengusai XML/JSON, HTML, CSS, PHP, MySQL, JavaScript</p>\r\n<p><strong>Kualifikasi :</strong></p>\r\n<p>&ndash; Pria/Wanita usia maksimal 30 tahun<br />&ndash; Mau bekerja keras dan mau berbagi hal-hal baru dibidangnya<br />&ndash; Siap bekerja dalam team maupun sendiri<br />&ndash; Siap ditarget</p>',5000000,0,1,'2018-07-11');
 
 /*Table structure for table `lowongan_komentar` */
 
@@ -131,11 +132,11 @@ CREATE TABLE `lowongan_komentar` (
   `konten` text NOT NULL,
   `tanggal` date NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
 
 /*Data for the table `lowongan_komentar` */
 
-insert  into `lowongan_komentar`(`id`,`lowongan_id`,`user_akun_id`,`konten`,`tanggal`) values (1,3,30,'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.','2018-07-13'),(3,3,38,'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut quisquam fugiat rem magni nemo, obcaecati maiores suscipit doloremque, aliquam consequuntur qui neque animi dolores ipsum dolorum ea velit? Ullam, nihil.','2018-07-13'),(4,3,30,'Lorem ipsum dolor sit amet, consectetur adipisicing elit,','2018-07-13');
+insert  into `lowongan_komentar`(`id`,`lowongan_id`,`user_akun_id`,`konten`,`tanggal`) values (1,3,30,'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.','2018-07-13'),(3,3,38,'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut quisquam fugiat rem magni nemo, obcaecati maiores suscipit doloremque, aliquam consequuntur qui neque animi dolores ipsum dolorum ea velit? Ullam, nihil.','2018-07-13'),(4,3,30,'Lorem ipsum dolor sit amet, consectetur adipisicing elit,','2018-07-13'),(5,3,1,'WTF!!!!!','2018-07-15');
 
 /*Table structure for table `pendidikan_formal` */
 
@@ -210,14 +211,14 @@ CREATE TABLE `profil_admin` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `user_akun_id` int(11) NOT NULL,
   `nip` varchar(20) NOT NULL,
-  `nama` varchar(30) NOT NULL,
+  `nama` varchar(100) NOT NULL,
   `jenis_kelamin` enum('Pria','Wanita') NOT NULL,
   `tempat_lahir` varchar(20) NOT NULL,
   `tanggal_lahir` date NOT NULL,
   `agama` enum('Islam','Katolik','Protestan','Hindu','Buddha','Lain-Lain') NOT NULL,
   `alamat` text NOT NULL,
   `telepon` varchar(12) NOT NULL,
-  `foto` varchar(100) NOT NULL,
+  `foto` varchar(100) NOT NULL DEFAULT 'default.png',
   `dibuat_pada` date NOT NULL,
   PRIMARY KEY (`id`),
   KEY `user_akun_id` (`user_akun_id`)
@@ -225,7 +226,7 @@ CREATE TABLE `profil_admin` (
 
 /*Data for the table `profil_admin` */
 
-insert  into `profil_admin`(`id`,`user_akun_id`,`nip`,`nama`,`jenis_kelamin`,`tempat_lahir`,`tanggal_lahir`,`agama`,`alamat`,`telepon`,`foto`,`dibuat_pada`) values (1,1,'131400036','Adi Raka Siwi, S.Kom','Pria','Padang','1994-04-29','Islam','Komplek Filano Mandiri BLOK A1/1 Tabing','081268280648','8eaa1da94969e2e4a3eb.jpg','2018-05-12');
+insert  into `profil_admin`(`id`,`user_akun_id`,`nip`,`nama`,`jenis_kelamin`,`tempat_lahir`,`tanggal_lahir`,`agama`,`alamat`,`telepon`,`foto`,`dibuat_pada`) values (1,1,'131400036','Syafrudin Siregar, S.Kom., M.Pd','Pria','Padang','1994-04-29','Islam','Komplek Filano Mandiri BLOK A1/1 Tabing','081268280648','af063cd827d662dc95f2.png','2018-05-12');
 
 /*Table structure for table `profil_pencaker` */
 
@@ -244,7 +245,7 @@ CREATE TABLE `profil_pencaker` (
   `telepon` varchar(12) DEFAULT NULL,
   `quote` varchar(100) DEFAULT NULL,
   `email` varchar(50) NOT NULL,
-  `photo` varchar(100) DEFAULT NULL,
+  `photo` varchar(100) DEFAULT 'default.png',
   `dibuat_pada` date NOT NULL,
   PRIMARY KEY (`id`),
   KEY `user_akun_id` (`user_akun_id`)
@@ -270,7 +271,7 @@ CREATE TABLE `profil_perusahaan` (
   `deskripsi_perusahaan` text,
   `website` varchar(50) DEFAULT NULL,
   `email` varchar(50) NOT NULL,
-  `logo_perusahaan` varchar(100) DEFAULT NULL,
+  `logo_perusahaan` varchar(100) DEFAULT 'default.png',
   `slogan` varchar(50) DEFAULT NULL,
   `dibuat_pada` date NOT NULL,
   PRIMARY KEY (`id`),
@@ -279,7 +280,7 @@ CREATE TABLE `profil_perusahaan` (
 
 /*Data for the table `profil_perusahaan` */
 
-insert  into `profil_perusahaan`(`id`,`user_akun_id`,`nama_perusahaan`,`no_siup`,`no_situ`,`bidang_usaha`,`alamat`,`telepon`,`deskripsi_perusahaan`,`website`,`email`,`logo_perusahaan`,`slogan`,`dibuat_pada`) values (2,38,'PT. Andalas Medika Infotama','436/6828A/338.8.12/2','0739/22-09/PK/VIII/0','Jasa','Komplek Cendana Mata Air Thp. VIII Blok A No. 4 Padang','444222','Membuat website','ami-tech.co.id','adiraka8@gmail.com','c6a0ccdf991102ee0e78.jpg','Membangun Negara','2018-07-08');
+insert  into `profil_perusahaan`(`id`,`user_akun_id`,`nama_perusahaan`,`no_siup`,`no_situ`,`bidang_usaha`,`alamat`,`telepon`,`deskripsi_perusahaan`,`website`,`email`,`logo_perusahaan`,`slogan`,`dibuat_pada`) values (2,38,'PT. Andalas Medika Infotama','436/6828A/338.8.12/2','0739/22-09/PK/VIII/0','Jasa','Komplek Cendana Mata Air Thp. VIII Blok A No. 4 Padang','444222','Membuat website','ami-tech.co.id','cs@ami-tech.co.id','c6a0ccdf991102ee0e78.jpg','Membangun Negara','2018-07-08');
 
 /*Table structure for table `user_akun` */
 
@@ -300,7 +301,7 @@ CREATE TABLE `user_akun` (
 
 /*Data for the table `user_akun` */
 
-insert  into `user_akun`(`id`,`hak_akses_id`,`username`,`password`,`status`,`aktivasi`,`dibuat_pada`) values (1,1,'admin','21232f297a57a5a743894a0e4a801fc3',1,1,'2018-05-11'),(2,2,'scrypto','21232f297a57a5a743894a0e4a801fc3',1,1,'2018-05-25'),(30,2,'zeddevil','0858dcffabb19cf27bcae1865f8530c5',1,1,'2018-06-05'),(38,3,'ami1234','20fd35afc7ae7cb8cc5c7a31b1101f3e',1,1,'2018-07-08');
+insert  into `user_akun`(`id`,`hak_akses_id`,`username`,`password`,`status`,`aktivasi`,`dibuat_pada`) values (1,1,'admin','21232f297a57a5a743894a0e4a801fc3',1,1,'2018-05-11'),(30,2,'zeddevil','0858dcffabb19cf27bcae1865f8530c5',1,1,'2018-06-05'),(38,3,'ami1234','20fd35afc7ae7cb8cc5c7a31b1101f3e',1,1,'2018-07-08');
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
