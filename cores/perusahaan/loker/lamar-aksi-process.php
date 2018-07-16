4b2d6dd16633ac69d80f4f4b6df46c266b558f67
Adi Raka Siwi <adiraka8@gmail.com>
2018-07-16 22:39:46 +0700

    asdasD

--- cores/perusahaan/loker/lamar-aksi-process.php
+++ cores/perusahaan/loker/lamar-aksi-process.php
@@ -4,6 +4,23 @@
 	$lamar_id = sanitizeThis($_GET['id']);
 	$val = sanitizeThis($_GET['val']);
 
-	var_dump($_GET);
+	// set query
+	$query = "UPDATE lamar SET status = '$val' WHERE id = '$lamar_id'";
+	$process = mysqli_query($conn, $query);
+
+	// notification feedback
+	if ($process) {
+		if ($val == '1') {
+			$_SESSION['sukses'] = 'Pelamar telah berhasil diterima!';
+		} elseif ($val == '2') {
+			$_SESSION['sukses'] = 'Pelamar telah berhasil ditolak!';
+		}
+		header('Location:?page=lihat-pelamar&id='.$lamar_id);
+		die();
+	} else {
+		$_SESSION['gagal'] = 'Telah terjadi kesalahan!';
+		header('Location:?page=lihat-pelamar&id='.$lamar_id);
+		die();
+	}
 
 ?>
